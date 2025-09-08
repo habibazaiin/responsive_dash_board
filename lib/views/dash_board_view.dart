becfefe855986e8cf5eac324a2d84e6f24833471
Habiba <zain.habiba0@gmail.com>
2025-09-08 17:05:00 +0300

    create custom drawer

---
+++ lib/views/dash_board_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout.dart';
+import 'package:responsive_dash_board/widgets/dash_board_desktop_layout.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desktopLayout: (context) => DashBoardDesktopLayout(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/dash_board_desktop_layout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+
+class DashBoardDesktopLayout extends StatelessWidget {
+  const DashBoardDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: CustomDrawer())
+      ],
+    );
+  }
+}
