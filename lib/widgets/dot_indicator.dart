439aee8d5a89eca8551154ccaf052327040ea8c3
Habiba <zain.habiba0@gmail.com>
2025-09-13 15:11:05 +0300

    create dot indicator

---
+++ lib/widgets/dot_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_dot.dart';
+
+class DotIndicator extends StatelessWidget {
+  const DotIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: 
+        List.generate(3, (index) => Padding(
+          padding: const EdgeInsets.only(right: 6.0),
+          child: CustomDot(isActive: false),
+        ))
+      ,
+    );
+  }
+}
