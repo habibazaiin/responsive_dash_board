b4cbc991872f7c3f866a55ba45632bae69c5e9f5
Habiba <zain.habiba0@gmail.com>
2025-09-13 19:04:56 +0300

    create income section header

---
+++ lib/widgets/income_section_header.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/range_option.dart';
+
+class IncomeSectionHeader extends StatelessWidget {
+  const IncomeSectionHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text('Income', style: AppStyles.styleSemiBold20(context)),
+        RangeOption(),
+      ],
+    );
+  }
+}
