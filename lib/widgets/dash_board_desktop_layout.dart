ccc9f032e39c216734b0108fa6cce0f4716107ba
Habiba <zain.habiba0@gmail.com>
2025-09-13 15:17:08 +0300

    create my carts section

--- lib/widgets/dash_board_desktop_layout.dart
+++ lib/widgets/dash_board_desktop_layout.dart
@@ -3,6 +3,7 @@ import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_sec
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/my_card.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
+import 'package:responsive_dash_board/widgets/my_cards_section.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
@@ -15,7 +16,7 @@ class DashBoardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(flex: 2, child: AllEpensesAndQuickInvoiceSection()),
         SizedBox(width: 24),
-        Expanded(child: MyCardsPageView()),
+        Expanded(child: MyCardsSection()),
       ],
     );
   }
