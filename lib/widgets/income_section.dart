eb527aac3a56e55082c7c99959d2a34576f475c3
Habiba <zain.habiba0@gmail.com>
2025-09-13 19:03:37 +0300

    create income section

---
+++ lib/widgets/income_section.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+
+class IncomeSection extends StatelessWidget {
+  const IncomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomBackgroundContainer(child: Column(
+      children: [
+        
+      ],
+    ));
+  }
+}
--- lib/widgets/my_card_and_transaction_history_section.dart
+++ lib/widgets/my_card_and_transaction_history_section.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
 import 'package:responsive_dash_board/widgets/my_card.dart';
+import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
+import 'package:responsive_dash_board/widgets/my_cards_section.dart';
 import 'package:responsive_dash_board/widgets/transaction_history.dart';
 
 class MyCardAndTransactionHistorySection extends StatelessWidget {
@@ -8,14 +10,14 @@ class MyCardAndTransactionHistorySection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomBackgroundContainer(child: 
-    Column(
-      children: [
-        MyCard(),
-        Divider(height: 40,
-        color: Color(0XFFF1F1F1),),
-        TransactionHistory()
-      ],
-    ));
+    return CustomBackgroundContainer(
+      child: Column(
+        children: [
+          MyCardsSection(),
+          Divider(height: 40, color: Color(0XFFF1F1F1)),
+          TransactionHistory(),
+        ],
+      ),
+    );
   }
 }
