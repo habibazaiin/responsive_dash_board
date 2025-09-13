1d0d61954e25ce87ca47a1a7633789f243c0fe36
Habiba <zain.habiba0@gmail.com>
2025-09-13 14:58:36 +0300

    create my carts page view

--- lib/widgets/dash_board_desktop_layout.dart
+++ lib/widgets/dash_board_desktop_layout.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/my_card.dart';
-import 'package:responsive_dash_board/widgets/quick_invoice.dart';
+import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
@@ -16,7 +15,7 @@ class DashBoardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(flex: 2, child: AllEpensesAndQuickInvoiceSection()),
         SizedBox(width: 24),
-        Expanded(child: MyCard()),
+        Expanded(child: MyCardsPageView()),
       ],
     );
   }
