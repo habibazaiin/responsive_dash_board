e89be5fb5c3bd4188bbad60c3973a087ced0a25e
Habiba <zain.habiba0@gmail.com>
2025-09-13 16:44:40 +0300

    create transaction history header

--- lib/widgets/transaction_history.dart
+++ lib/widgets/transaction_history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/transaction_history_header.dart';
 
 class TransactionHistory extends StatelessWidget {
   const TransactionHistory({super.key});
@@ -7,23 +8,18 @@ class TransactionHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Transaction History',
-              style: AppStyles.styleSemiBold20(context),
-            ),
-            Text(
-              'see all',
-              style: AppStyles.styleMedium16(
-                context,
-              ).copyWith(color: Color(0XFF4EB7F2)),
-            ),
-          ],
+        TransactionHistoryHeader(),
+        SizedBox(height: 20),
+        Text(
+          '13 April 2022',
+          style: AppStyles.styleMedium16(
+            context,
+          ).copyWith(color: Color(0XFFAAAAAA)),
         ),
       ],
     );
   }
 }
+
