0bba1c1e43c42f4ef35b71b91cd1c719a6a39104
Habiba <zain.habiba0@gmail.com>
2025-09-17 16:23:09 +0300

    add device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/dash_board_view.dart';
 
-void main() {
-  runApp(const ResponsiveDashBoard());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: true,
+    builder: (context) => ResponsiveDashBoard(), // Wrap your app
+  ),
+);
 
 class ResponsiveDashBoard extends StatelessWidget {
   const ResponsiveDashBoard({super.key});
@@ -12,6 +16,8 @@ class ResponsiveDashBoard extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: DashBoardView(),
     );
   }
